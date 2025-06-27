9a19631ac7fea48816b6915830b7975cfeccd77d
Guido Menendez <guidomenendez0@gmail.com>
2025-06-27 01:15:06 -0300

    replace async conc model for thread conc model

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import udp_server
-import asyncio
 
 def echo_upper_handler(data: bytes) -> bytes:
     return b"Echo: " + data.upper()
 
-server = udp_server.AsyncUDPServer(host="127.0.0.1", port=9999, buffer_size=1024, handler=echo_upper_handler)
+server = udp_server.UDPServer(host="127.0.0.1", port=9999, buffer_size=1024, handler=echo_upper_handler)
 
 try:
-    asyncio.run(server.serve())
+    server.serve()
 except KeyboardInterrupt:
     print("Server stopped by user.")
 
