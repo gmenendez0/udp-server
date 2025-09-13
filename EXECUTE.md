1e7c81adf648a7bc7d141b242aef00fae8d09ee6
sebastian <solaran@fi.uba.ar>
2025-09-13 17:42:02 -0300

    agrego capa de presentacion para handlear info del server

---
+++ EXECUTE.md
@@ -0,0 +1,11 @@
+# UDP Server – TP Redes FIUBA
+
+Servidor UDP en Python con capa de presentación (`argparse`).
+
+## Requisitos
+- Python 3.11 o superior
+
+
+## Ejecucion del servidor
+```bash
+python3.11 start_server.py -H 127.0.0.1 -p 9999 -v
---
+++ start_server.py
@@ -0,0 +1,46 @@
+import udp_server
+import asyncio
+import argparse
+
+def echo_upper_handler(data: bytes) -> bytes:
+    return b"Echo: " + data.upper()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog="udp_server", description="UDP server")
+
+    verbosity = parser.add_mutually_exclusive_group()
+    verbosity.add_argument("-v", "--verbose", action="store_true",
+                           help="increase output verbosity")
+    verbosity.add_argument("-q", "--quiet", action="store_true",
+                           help="decrease output verbosity")
+
+    parser.add_argument("-H", "--host", default="127.0.0.1",
+                        help="service IP address")
+    parser.add_argument("-p", "--port", type=int, default=9999,
+                        help="service port")
+
+    return parser.parse_args()
+
+
+
+def main():
+    args = parse_args()
+
+    if args.verbose:
+        print(f"[INFO] Iniciando servidor en {args.host}:{args.port}")
+    elif args.quiet:
+        pass  # no mostrar nada
+    else:
+        print(f"Servidor escuchando en {args.host}:{args.port}")
+
+    server = udp_server.AsyncUDPServer(host=args.host, port=args.port, buffer_size=1024, handler=echo_upper_handler)
+
+    try:
+        asyncio.run(server.serve())
+    except KeyboardInterrupt:
+        print("Server stopped by user.")
+
+
+if __name__ == "__main__":
+    main()
