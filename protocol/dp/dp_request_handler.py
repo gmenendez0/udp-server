511f1ae548010b880c0d3a2a1ff23057517e080f
sebastian <solaran@fi.uba.ar>
2025-09-27 15:18:55 -0300

    cambios para funcionamiento

--- protocol/dp/dp_request_handler.py
+++
@@ -1,5 +0,0 @@
-class DPRequestHandler:
-    def handle_request(request: bytes) -> bytes:
-       # Process the incoming request and return a response
-        response = f"Handled request: {request}"
-        return response
