e53b6c138cfc160d706d372cb6b30bb450a9e66f
sebastian <solaran@fi.uba.ar>
2025-09-28 18:13:53 -0300

    data_handler sencillo

--- protocol/data_handler/data_handler.py
+++ protocol/data_handler/data_handler.py
@@ -1,6 +1,15 @@
-from abc import ABC, abstractmethod
+from typing import Dict, Any
 
-class DataHandler(ABC):
-    @abstractmethod
-    def handle_data(self, data: bytes) -> bytes:
-        pass
+from protocol.rdt.rdt_connection import logger
+
+class DataHandler:
+    def __init__(self):
+        self.operation_count = 0
+        # Diccionario para almacenar paquetes por conexión
+        self.connection_packets = {}
+        self.connection_lock = {}
+    
+    def handle_data(self, data: bytes, context: Dict[str, Any] = None) -> bytes:
+        logger.info(f"Recibido paquete de datos: {data}")
+        return data
+        
